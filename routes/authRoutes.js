3d35427c7764b7f22862bec1898cd9b3846cae09
Isha Trivedi <ishat22705@gmail.com>
2026-06-09 19:45:25 +0530

    Task 3 - JWT Authentication and Authorization

---
+++ routes/authRoutes.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+const auth = require("../middleware/auth");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists"
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    await user.save();
+
+    res.status(201).json({
+      message: "User Registered Successfully"
+    });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User Not Found"
+      });
+    }
+
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid Credentials"
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.json({ token });
+
+   } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
+  }
+});
+
+router.get("/profile", auth, async (req, res) => {
+  res.json({
+    message: "Protected Route Accessed",
+    userId: req.user.id
+  });
+});
+
+module.exports = router;
